a156534f70c52b5de0061171e54a830ff7299382
Da-Jin Chu <dajinchu@gmail.com>
2020-12-07 17:04:36 -0500

    maybe fixed things

--- main.py
+++ main.py
@@ -4,27 +4,33 @@ import random
 import numpy as np
 import torch
 import torch.optim as optim
-from model import QNetworkFC
+from model import QNetworkFC, QNetwork
 
 import sys
 
-torch.manual_seed(0)
+torch.manual_seed(123456)
 
 DISCOUNT_FACTOR = 0.8
 EXPLORE_PROB = 0.2
-LEARNING_RATE = 0.01
+INIT_LEARNING_RATE = 0.2
+END_LEARNING_RATE = 0.02
 ITERATIONS = 1000
 MAX_MOVES = 100
 
+def valid_mask(enables):
+    # Make a mask of valid moves
+    mask = torch.zeros(64)
+    mask.scatter_(0, torch.LongTensor(enables), 1.)
+    return mask
 
 def train():
     env = gym.make('Reversi8x8-v0')
     env.reset()
     SKIP_ACTION = env.board_size**2 + 1
 
-    qnn = QNetworkFC()
+    qnn = QNetwork()
     qnn.double()
-    optimizer = optim.SGD(qnn.parameters(), lr=LEARNING_RATE)
+    optimizer = optim.SGD(qnn.parameters(), lr=INIT_LEARNING_RATE)
     criterion = torch.nn.MSELoss()
     # Given a state, return the evaluation of all actions from it
 
@@ -33,6 +39,11 @@ def train():
 
     wins, ties = 0, 0
     for i_episode in range(ITERATIONS):
+        if i_episode % (ITERATIONS/10) == 0:
+            step = i_episode / ITERATIONS
+            new_lr = INIT_LEARNING_RATE - step * (INIT_LEARNING_RATE-END_LEARNING_RATE)
+            print("LEARNING RATE", new_lr)
+            optimizer.param_groups[0]['lr'] = new_lr
         previous_eval = None
         previous_action = None
         observation = env.reset()
@@ -46,21 +57,8 @@ def train():
             else:
                 evaluation = Q(observation)
 
-                # Make a mask of valid moves
-                mask = torch.zeros(64)
-                mask.scatter_(0, torch.LongTensor(enables), 1.)
-
+                mask = valid_mask(enables)
                 max_q = torch.max(evaluation * mask)
-                # Adjust the q value
-                if previous_eval is not None:
-                    # Adjust NN to set Q(observation, a) = Qnew
-                    Qnew = reward + DISCOUNT_FACTOR * max_q
-                    new_eval = previous_eval * mask
-                    new_eval[previous_action] = Qnew
-                    optimizer.zero_grad()   # zero the gradient buffers
-                    loss = criterion(previous_eval, new_eval)
-                    loss.backward()
-                    optimizer.step()
 
                 should_explore = random.uniform(0, 1) < EXPLORE_PROB
                 if should_explore or torch.isnan(max_q) or max_q == 0:
@@ -68,9 +66,34 @@ def train():
                     action = random.choice(enables)
                 else:
                     action = torch.argmax(evaluation * mask)
-                previous_eval = evaluation.detach()
+                previous_eval = evaluation.clone()
                 previous_action = action
                 observation, reward, done, info = env.step(action)
+
+                # Adjust NN to set Q(observation, a) = Qnew
+                if(done):
+                    Qnew = torch.tensor((reward+1)/2) # scale range [-1,1] to [0,1]
+                else:
+                    max_q = torch.max(Q(observation) * mask)
+                    Qnew = DISCOUNT_FACTOR * max_q
+                new_eval = previous_eval.detach().clone()
+                new_eval[previous_action] = Qnew
+                optimizer.zero_grad()   # zero the gradient buffers
+                loss = criterion(previous_eval, new_eval)
+                loss.backward()
+                # print(reward, max_q, "Qnew", Qnew.detach().numpy(), "LOSS", loss.item())
+                optimizer.step()
+
+                # if done:
+                #     print("DONE")
+                #     Qnew = torch.tensor((reward+1)/2)
+                #     new_eval = previous_eval.detach()
+                #     new_eval[previous_action] = Qnew
+                #     # print(Qnew, Qprev, previous_eval)
+                #     optimizer.zero_grad()   # zero the gradient buffers
+                #     loss = criterion(previous_eval, new_eval)
+                #     loss.backward()
+                #     optimizer.step()
 
             if done:
                 print("Episode #{}/{} finished after {} timesteps".format(i_episode, ITERATIONS, t+1))
@@ -80,6 +103,8 @@ def train():
                     wins += 1
                 if reward == None:
                     ties += 1
+                if i_episode % 10 ==0:
+                    print("{}-{}-{} record. Win ratio: {}".format(wins, 1+i_episode-wins-ties, ties, wins/(1+i_episode-ties)))
                 break
     print("{}-{}-{} record. Win ratio: {}".format(wins, ITERATIONS-wins-ties, ties, wins/(ITERATIONS-ties)))
     return qnn
